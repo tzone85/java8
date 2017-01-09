7ba9503979b127ec8e27644346f2b54cdee4b618
Thando Mini <tzone85@gmail.com>
2017-01-09 17:35:49 +0200

    testing and refactoring

--- src/CodeBlock.java
+++ src/CodeBlock.java
@@ -5,8 +5,19 @@ public class CodeBlock {
 
     public static void main(String[] args) {
 
-        calculateScore(true, 800, 5, 100);
-        calculateScore(true, 10000, 8, 200);
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
     }
 
     public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
