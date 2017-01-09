26161cecb0615efc53e965615835f01b5fe0e586
Thando Mini <tzone85@gmail.com>
2017-01-09 17:18:51 +0200

    initial commit

---
+++ src/CodeBlock.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Thando Mini on 1/9/2017.
+ */
+public class CodeBlock {
+
+    public static void main(String[] args) {
+
+        calculateScore(true, 800, 5, 100);
+        calculateScore(true, 10000, 8, 200);
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+
+        if(gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
+            System.out.println("Your final score was "+ finalScore);
+        }
+    }
+
+}
